2da84debe415c6af89c000ba86c9b3b323ce0bcb
Teerapat <teera123@me.com>
2018-04-01 21:04:01 +0700

    add http listen to prevent heroku killed process

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"context"
+	"net/http"
 
 	"github.com/BeepBoopHQ/go-slackbot"
 	"github.com/nlopes/slack"
@@ -11,13 +12,16 @@ import (
 func main()  {
 	bot := slackbot.New(os.Getenv("SLACK_TOKEN"))
 
-	toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
-	toMe.Hear("(?i)(หวัดดี|ดีจ้า|สวัสดี).*").MessageHandler(helloHandler)
+	//toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
+	//toMe.Hear("(?i)(หวัดดี|ดีจ้า|สวัสดี).*").MessageHandler(helloHandler)
 
 	bot.Hear("(?i)(หวัดดี|ดีจ้า|สวัสดี).*").MessageHandler(helloHandler)
 	bot.Run()
+
+	// heroku requires the process to bind port or it is killed
+	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
 }
 
-func helloHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
+func helloHandler(_ context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
 	bot.Reply(evt, "สวัสดีค่ะ ヾ（〃＾∇＾）ﾉ♪", slackbot.WithTyping)
 }
